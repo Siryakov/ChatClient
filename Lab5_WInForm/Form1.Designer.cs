a649c5bb74b8366c269ebac4b6abb7add38261f5
Kirill Siryakov <SiryakovJob@gmail.com>
2023-06-17 20:38:19 +0300

    Добавьте файлы проекта.

---
+++ Lab5_WInForm/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Lab5_WInForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(21, 393);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(126, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect to Server";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(21, 97);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(195, 23);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(242, 96);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(398, 34);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(311, 382);
+            this.txtOutput.TabIndex = 4;
+            this.txtOutput.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите cообщение:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnConnect;
+        private TextBox txtMessage;
+        private Button btnSend;
+        private RichTextBox txtOutput;
+        private Label label1;
+    }
+}
---
+++ Lab5_WInForm/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5_WInForm
+{
+    public partial class Form1 : Form
+    {
+        private Socket client;
+        private byte[] buffer = new byte[1024];
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+
+        {
+            
+            
+            try
+            {
+                var ipAddress = IPAddress.Parse("127.0.0.1");
+                var port = 8005;
+                client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                await client.ConnectAsync(new IPEndPoint(ipAddress, port));
+                AddText($"Client connected to {ipAddress}:{port}");
+                _ = Task.Run(ReceiveLoop);
+            }
+            catch (Exception ex)
+            {
+                txtOutput.AppendText($"Error connecting to server: {ex.Message}");
+            }
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var message = txtMessage.Text;
+                var messageBytes = Encoding.UTF8.GetBytes(message);
+                _ = await client.SendAsync(messageBytes, SocketFlags.None);
+                txtOutput.AppendText($"Client sent message: \"{message}\"");
+                txtMessage.Clear();
+            }
+            catch (Exception ex)
+            {
+                txtOutput.AppendText($"Error sending message: {ex.Message}");
+            }
+        }
+
+        private async Task ReceiveLoop()
+        {
+            try
+            {
+                while (true)
+                {
+                    var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+                    var response = Encoding.UTF8.GetString(buffer, 0, received);
+                    if (response == "")
+                    {
+                        AddText($"Client received acknowledgment: \"{response}\"");
+                        break;
+                    }
+                    else
+                    {
+                        AddText($"Client received message: \"{response}\"");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                txtOutput.AppendText($"Error receiving message: {ex.Message}");
+            }
+        }
+
+        private void AddText(string text)
+        {
+            txtOutput.Text += (text + "\n");
+        
+        }
+
+       
+    }
+}
